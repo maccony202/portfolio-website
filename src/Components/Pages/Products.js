acf3bfe1c60b61b5b1bd1a342b7c20d77cb1c669
Collins Nana Antwi <nanastef10@hotmail.com>
2023-02-04 10:52:47 +0000

    Add new changes

--- src/Components/Pages/Products.js
+++ src/Components/Pages/Products.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import ImgPlaceholder from '../assets/images/awesomebooks.png'
 
 const Projects = () => {
+    const [showModal, setShowModal] = useState(false)
     return(
         <div>
             <div className="projects">
@@ -28,7 +29,31 @@ const Projects = () => {
                         <li>bootstrap</li>
                         <li>ruby</li>
                     </ul>
-                    <button className="see-proj-btn">See project</button>
+                    <button onClick={() => setShowModal(true)} className="see-proj-btn">See project</button>
+                    {showModal && (
+                        <div className="my-modal">
+                            <button onClick={() => setShowModal(false)}>close</button>
+                            <div className="modal-content">
+                            <img className="proj-img1" src={ImgPlaceholder} alt='project1'/>
+                    <div>
+                      
+                    <p className="title-post">Multi-Post Stories</p>
+                    <p className="supporting-text">
+                    A daily selection of privately personalized reads; no accounts or sign-ups required. 
+                    has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a standard dummy text.
+                    </p>
+                    <ul className="tags">
+                        <li>css</li>
+                        <li>html</li>
+                        <li>bootstrap</li>
+                        <li>ruby</li>
+                    </ul>
+                      </div>
+                            </div>
+
+                        </div>
+                    )}
                     </div>
                   
                 </div>
