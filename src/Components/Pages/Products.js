14802a5d512ea1b12419bbf3726137bce4b2af02
Collins Nana Antwi <nanastef10@hotmail.com>
2023-02-04 16:16:54 +0000

    Add new changes to projects section

--- src/Components/Pages/Products.js
+++ src/Components/Pages/Products.js
@@ -32,25 +32,27 @@ const Projects = () => {
                     <button onClick={() => setShowModal(true)} className="see-proj-btn">See project</button>
                     {showModal && (
                         <div className="my-modal">
-                            <button onClick={() => setShowModal(false)}>close</button>
+                            <button className="close-btn" onClick={() => setShowModal(false)}>close</button>
                             <div className="modal-content">
-                            <img className="proj-img1" src={ImgPlaceholder} alt='project1'/>
-                    <div>
-                      
-                    <p className="title-post">Multi-Post Stories</p>
-                    <p className="supporting-text">
+                            <img className="pop-img1" src={ImgPlaceholder} alt='project1'/>
+                    <div className="proj-details">
+                    <ul className="tags">
+                        <li>css</li>
+                        <li>html</li>
+                        <li>JavaScript</li>
+                    </ul>
+                    <p className="pop-title">Multi-Post Stories</p>
+                    <p className="proj-cont">
                     A daily selection of privately personalized reads; no accounts or sign-ups required. 
                     has been the industry's standard dummy text ever since the 1500s, 
                     when an unknown printer took a standard dummy text.
                     </p>
-                    <ul className="tags">
-                        <li>css</li>
-                        <li>html</li>
-                        <li>bootstrap</li>
-                        <li>ruby</li>
-                    </ul>
+                    <div className="pop-btn">
+                        <button className="proj-pop-btn">See live</button>
+                        <button className="proj-pop-btn">See source</button>
+                    </div>
                       </div>
-                            </div>
+                        </div>
 
                         </div>
                     )}
